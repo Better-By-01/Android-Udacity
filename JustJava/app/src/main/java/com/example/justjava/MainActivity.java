3791707bc04204c3ee95a461de501e419f254582
Ashutosh-Singh-Patel <patelsinghashutosh@gmail.com>
2021-12-09 23:41:07 +0530

    JustJava completed

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -8,9 +8,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.NumberFormat;
+
 /**This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void decrement(View view)    {
+        if (quantity>0) quantity--;
+        display(quantity);
+    }
+
+    public void increment(View view)    {
+        if(quantity>=0) quantity++;
+        display(quantity);
+    }
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        if (quantity>0) {
+            int price = quantity*5;
+            String priceMessage = "Total: $" + price + "\nThank You !!!";
+            displayMessage(priceMessage);
+        }
+        else
+            displayPrice(quantity);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
@@ -31,5 +54,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
